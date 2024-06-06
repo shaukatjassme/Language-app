379ad7dd4895dc72daa14e5ea63dea9bf4ef8e26
Shaukatjassme <shaukat.jassme786@gmail.com>
2024-06-06 15:31:55 +0500

    app-updated

--- src/index.js
+++ src/index.js
@@ -1,25 +1,10 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-
-const el = document.getElementById('root');
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
+const el = document.getElementById("root");
 
 const root = ReactDOM.createRoot(el);
 
-function App() {
- 
-  const config={color:"red"};
 
-
-    return(
-    <div>
-       <h1> My color is {config.color}</h1>
-       
-        
-    </div> 
-    )
-}
 
 root.render(<App />);
-
-
-
